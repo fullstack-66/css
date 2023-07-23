9d8b61984ec0e9e649684ad278a8fa57ff03b6db
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-07-24 06:18:55 +0700

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+"use client";
+
+import styled from "styled-components";
+
 export default function Home() {
-  return <></>;
+  const Button = styled.button`
+    color: grey;
+  `;
+  return (
+    <>
+      <Button>Click me</Button>
+    </>
+  );
 }
