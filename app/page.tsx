00162adca92e48fcc792c2db5ac2df07978a959f
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-07-23 19:20:02 +0700

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+import styles1 from "./style1.module.css";
+import styles2 from "./style2.module.css";
 export default function Home() {
-  return <></>;
+  return (
+    <div>
+      <div
+        style={{
+          height: "25vh",
+          width: "300px",
+          display: "flex",
+          backgroundColor: "green",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <span style={{ color: "white" }}>Inline Style</span>
+      </div>
+      <div className={styles1.card}>
+        <span className={styles1.header}>CSS Module 1</span>
+      </div>
+      <div className={styles2.card}>
+        <span className={styles2.header}>CSS Module 2</span>
+      </div>
+    </div>
+  );
 }
