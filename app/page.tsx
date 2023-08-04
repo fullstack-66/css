5556581ce221d1b6a15c190a30a5e06fccd4034b
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-08-05 05:45:34 +0700

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,44 @@
 import styled from "styled-components";
 
 export default function Home() {
-  const Button = styled.button`
-    color: grey;
+  const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1em;
   `;
+
+  const Title = styled.div`
+    font-size: 1.5em;
+    color: #bf4f74;
+  `;
+
+  interface ButtonProps {
+    $primary?: boolean;
+  }
+
+  const Button = styled.button<ButtonProps>`
+    font-size: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid #bf4f74;
+    border-radius: 1em;
+    cursor: pointer;
+
+    /* Adapt the colors based on primary prop */
+    background: ${(props) => (props.$primary ? "#BF4F74" : "white")};
+    color: ${(props) => (props.$primary ? "white" : "#BF4F74")};
+    &:hover {
+      background: ${(props) => (props.$primary ? "white" : "#BF4F74")};
+      color: ${(props) => (props.$primary ? "#BF4F74" : "white")};
+    }
+  `;
+
   return (
-    <>
+    <Container>
+      <Title>Hello World!</Title>
+      <Button $primary>Click me</Button>
       <Button>Click me</Button>
-    </>
+    </Container>
   );
 }
