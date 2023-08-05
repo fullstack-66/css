2617c72fa4b78c482b4ab4938f08b28b923ebce3
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-08-05 10:07:57 +0700

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <></>;
-}
+"use client";
+import * as Popover from "@radix-ui/react-popover";
+import "./styles.css";
+const Home = () => (
+  <Popover.Root>
+    <Popover.Trigger className="PopoverTrigger">Show info</Popover.Trigger>
+    <Popover.Portal>
+      <Popover.Content className="PopoverContent">
+        Some content
+        <Popover.Arrow className="PopoverArrow" />
+      </Popover.Content>
+    </Popover.Portal>
+  </Popover.Root>
+);
+
+export default Home;
