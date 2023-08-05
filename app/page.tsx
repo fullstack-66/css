760353d67fe8544b4276a42db005825d12da0a7f
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2023-08-05 11:59:16 +0700

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,47 @@
+"use client";
+import { FC, useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 export default function Home() {
-  return <></>;
+  const [open, setOpen] = useState(false);
+  const onOpen = () => setOpen(true);
+  const onClose = () => setOpen(false);
+  return (
+    <div className="container mx-auto flex flex-col gap-2 items-center">
+      <div className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        shadcn/ui
+      </div>
+      <Button onClick={onOpen}>Click Me</Button>
+      <Button variant={"destructive"} onClick={onOpen}>
+        Click Me
+      </Button>
+      <ModalComponent open={open} onClose={onClose} />
+    </div>
+  );
 }
+
+const ModalComponent: FC<{ open: boolean; onClose: () => void }> = ({
+  open,
+  onClose,
+}) => {
+  return (
+    <Dialog open={open} onOpenChange={onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
